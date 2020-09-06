da2a4c4bff834a41363cce7151b4b811fc5910e2
Igor <efimov.036@gmail.com>
2020-09-06 12:08:21 +0700

    moved some components generation into separate functions

--- src/components/editAndCreate.js
+++ src/components/editAndCreate.js
@@ -1,57 +1,71 @@
-const createEditAndCreateTaskCardComponent = (task) => {
+const createColorsComponent = () => {
+  return `
+  <input
+                type="radio"
+                id="color-black-4"
+                class="card__color-input card__color-input--black visually-hidden"
+                name="color"
+                value="black"
+              />
+              <label
+                for="color-black-4"
+                class="card__color card__color--black"
+                >black</label
+              >
+              <input
+                type="radio"
+                id="color-yellow-4"
+                class="card__color-input card__color-input--yellow visually-hidden"
+                name="color"
+                value="yellow"
+                checked
+              />
+              <label
+                for="color-yellow-4"
+                class="card__color card__color--yellow"
+                >yellow</label
+              >
+              <input
+                type="radio"
+                id="color-blue-4"
+                class="card__color-input card__color-input--blue visually-hidden"
+                name="color"
+                value="blue"
+              />
+              <label
+                for="color-blue-4"
+                class="card__color card__color--blue"
+                >blue</label
+              >
+              <input
+                type="radio"
+                id="color-green-4"
+                class="card__color-input card__color-input--green visually-hidden"
+                name="color"
+                value="green"
+              />
+              <label
+                for="color-green-4"
+                class="card__color card__color--green"
+                >green</label
+              >
+              <input
+                type="radio"
+                id="color-pink-4"
+                class="card__color-input card__color-input--pink visually-hidden"
+                name="color"
+                value="pink"
+              />
+              <label
+                for="color-pink-4"
+                class="card__color card__color--pink"
+                >pink</label
+              >`
+};
 
-  const {} = task;
-
-  const description = `Example task with default color.`;
-  const dueDate = `26 September`;
-  const dueTime = `16:15`;
-  const color = `black`;
-  const isRepeat = false;
-  const isDeadlined = false;
-
-  const repeatClass = isRepeat ? `card--repeat` : ``;
-  const deadlineClass = isDeadlined ? `card--deadline` : ``;
-
-  return (`<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
-  <form class="card__form" method="get">
-    <div class="card__inner">
-      <div class="card__color-bar">
-          <svg class="card__color-bar-wave" width="100%" height="10">
-            <use xlink:href="#wave"></use>
-          </svg>
-      </div>
-      <div class="card__textarea-wrap">
-          <label>
-            <textarea
-              class="card__text"
-              placeholder="Start typing your text here..."
-              name="text"
-            >${description}</textarea>
-          </label>
-      </div>
-      <div class="card__settings">
-          <div class="card__details">
-            <div class="card__dates">
-              <button class="card__date-deadline-toggle" type="button">
-                date: <span class="card__date-status">yes</span>
-              </button>
-              <fieldset class="card__date-deadline">
-                <label class="card__input-deadline-wrap">
-                  <input
-                    class="card__date"
-                    type="text"
-                    placeholder=""
-                    name="date"
-                    value="${dueDate} ${dueTime}"
-                  />
-                </label>
-              </fieldset>
-              <button class="card__repeat-toggle" type="button">
-                repeat:<span class="card__repeat-status">yes</span>
-              </button>
-              <fieldset class="card__repeat-days">
-                <div class="card__repeat-days-inner">
-                  <input
+const createRepeatingDaysComponent = () => {
+  return `
+  <input
                     class="visually-hidden card__repeat-day-input"
                     type="checkbox"
                     id="repeat-mo-4"
@@ -123,7 +137,63 @@ const createEditAndCreateTaskCardComponent = (task) => {
                   />
                   <label class="card__repeat-day" for="repeat-su-4"
                     >su</label
-                  >
+                  >`
+};
+
+const createEditAndCreateTaskCardComponent = (task) => {
+
+  const {} = task;
+
+  const description = `Example task with default color.`;
+  const dueDate = `26 September`;
+  const dueTime = `16:15`;
+  const color = `black`;
+  const isRepeat = false;
+  const isDeadlined = false;
+
+  const repeatClass = isRepeat ? `card--repeat` : ``;
+  const deadlineClass = isDeadlined ? `card--deadline` : ``;
+
+  return (`<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
+  <form class="card__form" method="get">
+    <div class="card__inner">
+      <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+      </div>
+      <div class="card__textarea-wrap">
+          <label>
+            <textarea
+              class="card__text"
+              placeholder="Start typing your text here..."
+              name="text"
+            >${description}</textarea>
+          </label>
+      </div>
+      <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <button class="card__date-deadline-toggle" type="button">
+                date: <span class="card__date-status">yes</span>
+              </button>
+              <fieldset class="card__date-deadline">
+                <label class="card__input-deadline-wrap">
+                  <input
+                    class="card__date"
+                    type="text"
+                    placeholder=""
+                    name="date"
+                    value="${dueDate} ${dueTime}"
+                  />
+                </label>
+              </fieldset>
+              <button class="card__repeat-toggle" type="button">
+                repeat:<span class="card__repeat-status">yes</span>
+              </button>
+              <fieldset class="card__repeat-days">
+                <div class="card__repeat-days-inner">
+                  ${createRepeatingDaysComponent()}
                 </div>
               </fieldset>
             </div>
@@ -131,67 +201,7 @@ const createEditAndCreateTaskCardComponent = (task) => {
         <div class="card__colors-inner">
             <h3 class="card__colors-title">Color</h3>
             <div class="card__colors-wrap">
-              <input
-                type="radio"
-                id="color-black-4"
-                class="card__color-input card__color-input--black visually-hidden"
-                name="color"
-                value="black"
-              />
-              <label
-                for="color-black-4"
-                class="card__color card__color--black"
-                >black</label
-              >
-              <input
-                type="radio"
-                id="color-yellow-4"
-                class="card__color-input card__color-input--yellow visually-hidden"
-                name="color"
-                value="yellow"
-                checked
-              />
-              <label
-                for="color-yellow-4"
-                class="card__color card__color--yellow"
-                >yellow</label
-              >
-              <input
-                type="radio"
-                id="color-blue-4"
-                class="card__color-input card__color-input--blue visually-hidden"
-                name="color"
-                value="blue"
-              />
-              <label
-                for="color-blue-4"
-                class="card__color card__color--blue"
-                >blue</label
-              >
-              <input
-                type="radio"
-                id="color-green-4"
-                class="card__color-input card__color-input--green visually-hidden"
-                name="color"
-                value="green"
-              />
-              <label
-                for="color-green-4"
-                class="card__color card__color--green"
-                >green</label
-              >
-              <input
-                type="radio"
-                id="color-pink-4"
-                class="card__color-input card__color-input--pink visually-hidden"
-                name="color"
-                value="pink"
-              />
-              <label
-                for="color-pink-4"
-                class="card__color card__color--pink"
-                >pink</label
-              >
+            ${createColorsComponent()}
             </div>
           </div>
       </div>
